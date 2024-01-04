9efa82161c2479022b200d154b62df3d91c61fd7
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2024-01-04 14:14:13 -0500

    began react exercises

---
+++ Front End Development Libraries/js/js.js
@@ -0,0 +1,9 @@
+const JSX = <div>
+    <h1>Hello JSX!</h1>
+<p>This is my first JSX project</p>
+    <ul>
+        <li>Writes in JavaScript</li>
+        <li>Uses HTML to create a webpage</li>
+        <li>Then creates the page</li>
+    </ul>
+</div>;
