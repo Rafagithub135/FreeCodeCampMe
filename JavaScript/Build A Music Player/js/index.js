566c9a7ef0ca748cfe978d3e0ceda6f0cfe8dba7
Ralph M. <102826377+Rafagithub135@users.noreply.github.com>
2024-06-15 09:41:19 -0400

    troubleshot error for song display

--- JavaScript/Build A Music Player/js/index.js
+++ JavaScript/Build A Music Player/js/index.js
@@ -131,9 +131,9 @@ const playNextSong = () => {
 };
 
 const playPreviousSong = () => {
-    if (userData?.currentSong === null) {
+    if (userData?.currentSong === null)
         return;
-    } else {
+    else {
         const currentSongIndex = getCurrentSongIndex();
         const previousSong = userData?.songs[currentSongIndex - 1];
         playSong(previousSong.id);
@@ -151,16 +151,16 @@ const shuffle = () => {
 };
 
 const deleteSong = (id) => {
-    userData.songs = userData?.songs.filter(song => song.id !== id);
-    renderSongs(userData?.songs);
-    highlightCurrentSong();
-    setPlayButtonAccessibleText();
     if (userData?.currentSong?.id === id) {
         userData.currentSong = null;
         userData.songCurrentTime = 0;
         pauseSong();
         setPlayerDisplay();
     }
+    userData.songs = userData?.songs.filter((song) => song.id !== id);
+    renderSongs(userData?.songs);
+    highlightCurrentSong();
+    setPlayButtonAccessibleText();
     if (userData?.songs.length === 0) {
         const resetButton = document.createElement("button");
         const resetText = document.createTextNode("Reset Playlist");
@@ -188,29 +188,28 @@ const setPlayerDisplay = () => {
 
 const highlightCurrentSong = () => {
     const playlistSongElements = document.querySelectorAll(".playlist-song");
+    const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
     playlistSongElements.forEach((songEl) => {
         songEl.removeAttribute("aria-current");
     });
-    if (songToHighlight) {
-        songToHighlight.setAttribute("aria-current", "true");
-    }
-    const currentSong = document.getElementById(`song-${userData?.currentSong.id}`);
-    currentSong.classList.add("current-song");
+    if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
 };
 
-const renderSongs = array => {
+const renderSongs = (array) => {
 // The map() method takes a function as an argument. This is called a callback function, which is a function that is passed to another function as an argument. The map() method then returns a new array with the results.
     const songsHTML = array.map((song) => {
-        return `<li class="playlist-song" id="song-${song.id}">
-<button onclick="playSong(${song.id})" class="playlist-song-info"><span class="playlist-song-title">${song.title}</span>
-<span class="playlist-song-artist">${song.artist}</span>
-<span class="playlist-song-duration">${song.duration}</span>
-</button>
-<button onclick="deleteSong(${song.id})" class="playlist-song-delete" aria-label="Delete${song.title}"><svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
-</button>.onclick = (playSong(${song.id});
-</li>`;
+        return `<li id="song-${song.id}" class="playlist-song">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
+          <span class="playlist-song-title">${song.title}</span>
+          <span class="playlist-song-artist">${song.artist}</span>
+          <span class="playlist-song-duration">${song.duration}</span>
+      </button>
+      <button onclick="deleteSong(${song.id})" class="playlist-song-delete" aria-label="Delete ${song.title}">
+          <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
+        </button>
+      </li>`;
     }).join("");
-
     playlistSongs.innerHTML = songsHTML;
 };
 
@@ -219,9 +218,7 @@ const setPlayButtonAccessibleText = () => {
     playButton.setAttribute("aria-label", song?.title ? `Play ${song.title}` : "Play");
 };
 
-const getCurrentSongIndex = () => {
-    return userData?.songs.findIndex(song => song.id === userData?.currentSong.id);
-};
+const getCurrentSongIndex = () => userDAta?.songs.indexOf(userData?.currentSong);
 
 playButton.addEventListener("click", () => {
     if (userData?.currentSong === null) {
@@ -239,8 +236,6 @@ previousButton.addEventListener("click", playPreviousSong);
 
 shuffleButton.addEventListener("click", shuffle);
 
-renderSongs(sortSongs());
-
 const sortSongs = () => {
     userData?.songs.sort((a, b) => {
         if (a.title < b.title) {
@@ -253,3 +248,6 @@ const sortSongs = () => {
     });
     return userData?.songs;
 };
+
+renderSongs(sortSongs());
+setPlayButtonAccessibleText();
